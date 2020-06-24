e33585e2c30152573406647aa627ea9cf1028adf
Manimaran E <manimarane@pc-65-76.customer.ask4.lan>
2020-06-24 19:45:38 +0200

    added some comments to the method

--- subset_superset_example.py
+++ subset_superset_example.py
@@ -4,10 +4,10 @@ sample_tuple = (2, 4, 6, 10)
 sample_set2 = set(sample_tuple)
 print(sample_set2)
 
-if sample_set2.issubset(sample_set1):
+if sample_set2.issubset(sample_set1): # tests if every element in set2 is in set1
     print("sample_set2 is a subset of sample_set1")
 
 print("**" * 10)
 
-if sample_set1.issuperset(sample_set2):
+if sample_set2.issuperset(sample_set2): # tests if every element in set1 is in set2
     print("sample_set1 is a superset of sample_set2")
