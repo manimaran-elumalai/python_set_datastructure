762df5db05e2528d771e9303b8d01aef51a62749
Manimaran E <manimarane@pc-65-76.customer.ask4.lan>
2020-06-24 19:42:56 +0200

    creating a frozen set with

---
+++ frozenset_method_example.py
@@ -0,0 +1,29 @@
+#  These sets are immutable and you cannot add, remove/discard anything to it
+
+sample_set1 = frozenset(range(0, 30, 3))
+print(sample_set1)
+print(len(sample_set1))
+
+#  sample_set1.add(5) - AttributeError: 'frozenset' object has no attribute 'add'
+
+sample_set2 = {21, 9, 12}
+print(sample_set2)
+print(len(sample_set2))
+
+sample_set3 = set([9, 12])
+
+print(sample_set1.union(sample_set2)) # creates a new set with elements from both the sets
+print(len(sample_set1.union(sample_set2)))
+print(sample_set1.intersection(sample_set2)) # creates a new set with common elements of both sets
+print(len((sample_set1.intersection(sample_set2))))
+print(sample_set1.difference(sample_set2)) # creates a new set with elements in a and not in b
+print(len(sample_set1.difference(sample_set2)))
+print(sample_set1.symmetric_difference(sample_set2)) # creates a new set with elements in either a or b but not in both
+print(len(sample_set1.symmetric_difference(sample_set2)))
+
+
+if sample_set2.issubset(sample_set1):
+    print("it is subset")
+
+if sample_set1.issuperset(sample_set2):
+    print("yes it is superset")
